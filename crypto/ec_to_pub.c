167b1d58687a0dd25970fea1a2c997d6b2ed0106
Akhigbesimeon <s.akhigbe@alustudent.com>
2026-05-09 19:32:58 +0200

    ec_to_pub.c file added

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -1,50 +1,40 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_from_pub - Creates EC_KEY from public key
- * @pub: Buffer containing public key
- *
- * Return: Pointer to created EC_KEY structure, NULL on error
+ * ec_to_pub - Extracts public key from EC_KEY structure
+ * @key: EC_KEY structure containing key pair
+ * @pub: Buffer to store extracted public key
+ * Return: Pointer to pub buffer or NULL on error
  */
-EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
-	EC_KEY *key = NULL;
-	EC_POINT *point = NULL;
-	const EC_GROUP *group = NULL;
+	const EC_POINT *point;
+	const EC_GROUP *group;
+	size_t len;
 
-	if (!pub)
+	if (!key || !pub)
+	{
 		return (NULL);
+	}
 
-	key = EC_KEY_new_by_curve_name(EC_CURVE);
-	if (!key)
+	point = EC_KEY_get0_public_key(key);
+	if (!point)
+	{
 		return (NULL);
+	}
 
 	group = EC_KEY_get0_group(key);
 	if (!group)
 	{
-		EC_KEY_free(key);
 		return (NULL);
 	}
 
-	point = EC_POINT_new(group);
-	if (!point)
+	len = EC_POINT_point2oct(group, point, POINT_CONVERSION_UNCOMPRESSED,
+				 pub, EC_PUB_LEN, NULL);
+	if (len != EC_PUB_LEN)
 	{
-		EC_KEY_free(key);
-		return (NULL);
-	}
-	if (!EC_POINT_oct2point(group, point, pub, EC_PUB_LEN, NULL))
-	{
-		EC_POINT_free(point);
-		EC_KEY_free(key);
 		return (NULL);
 	}
 
-	if (!EC_KEY_set_public_key(key, point))
-	{
-		EC_POINT_free(point);
-		EC_KEY_free(key);
-		return (NULL);
-	}
-	EC_POINT_free(point);
-	return (key);
+	return (pub);
 }
