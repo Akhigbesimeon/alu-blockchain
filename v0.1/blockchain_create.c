e043384710e05b334052f3977033927903d582de
Akhigbesimeon <s.akhigbe@alustudent.com>
2026-05-11 17:45:49 +0200

    blockchain_create.c file added

--- v0.1/blockchain_create.c
+++ v0.1/blockchain_create.c
@@ -1,29 +1,51 @@
 #include "blockchain.h"
 
+#define HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
+
 /**
- * block_create - creates a Block structure and initializes it
+ * blockchain_create - creates a Blockchain structure, and initializes it
  *
- * @prev: pointer to the previous Block in the Blockchain
- * @data: pointer to a memory area to duplicate in the Block’s data
- * @data_len: number of bytes to duplicate in data
- *
- * Return: pointer to the allocated Block, or NULL upon failure
+ * Return: a pointer to the created blockchain
  */
-block_t *block_create(block_t const *prev, int8_t const *data,
-
-		      uint32_t data_len)
+blockchain_t *blockchain_create(void)
 {
-	block_t *block;
+	blockchain_t *blockchain = NULL;
+	block_t *genesis_block = NULL;
+	llist_t *llist = NULL;
 
-	block = calloc(1, sizeof(*block));
-	if (block == NULL)
+	genesis_block = calloc(1, sizeof(*genesis_block));
+	if (genesis_block == NULL)
 		return (NULL);
-	block->info.index = prev->info.index + 1;
-	block->info.difficulty = 0;
-	block->info.timestamp = time(NULL);
-	block->info.nonce = 0;
-	memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
-	memcpy(block->data.buffer, data, data_len);
-	block->data.len = data_len;
-	return (block);
+
+	genesis_block->info.index = 0;
+	genesis_block->info.difficulty = 0;
+	genesis_block->info.timestamp = 1537578000;
+	genesis_block->info.nonce = 0;
+	memset(genesis_block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
+	memcpy(genesis_block->data.buffer, "Holberton School", 16);
+	genesis_block->data.len = 16;
+	memcpy(genesis_block->hash, HASH, SHA256_DIGEST_LENGTH);
+
+	llist = llist_create(MT_SUPPORT_TRUE);
+	if (llist == NULL)
+	{
+		free(genesis_block);
+		return (NULL);
+	}
+
+	if (llist_add_node(llist, genesis_block, ADD_NODE_FRONT) != 0)
+	{
+		llist_destroy(llist, 1, NULL), free(genesis_block);
+		return (NULL);
+	}
+
+	blockchain = calloc(1, sizeof(*blockchain));
+	if (blockchain == NULL)
+	{
+		llist_destroy(llist, 1, NULL), free(genesis_block);
+		return (NULL);
+	}
+	blockchain->chain = llist;
+
+	return (blockchain);
 }
