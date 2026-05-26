ea1fbb576e2bc22e0f140914f8ccfb71374d48f0
Akhigbesimeon <s.akhigbe@alustudent.com>
2026-05-26 17:23:38 +0200

    pushing

--- blockchain/v0.2/block_hash.c
+++ blockchain/v0.2/block_hash.c
@@ -8,12 +8,11 @@
  *
  * Return: pointer to the hash_buf
  */
-uint8_t *block_hash(block_t const *block,
-
-		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+uint8_t *
+block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	/*hash will contain a digest of both the Block info and data*/
-	size_t len = sizeof(block->info) + block->data.len;
+	/*hash will contain a digest of both the Block info and data */
+	size_t len = sizeof (block->info) + block->data.len;
 
-	return (sha256((int8_t const *)block, len, hash_buf));
+	return (sha256((int8_t const *) block, len, hash_buf));
 }
