8435f8f529e3b0b0657bab215bcdbe7535fb1232
Akhigbesimeon <s.akhigbe@alustudent.com>
2026-05-26 18:03:42 +0200

    block_mine.c file edited

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -1,4 +1,5 @@
 #include "blockchain.h"
+#include <stdio.h>
 
 /**
  * block_mine - mines a block with requisite difficulty
@@ -7,22 +8,27 @@
 void
 block_mine(block_t *block)
 {
-	/* nonce is a 64-bit number that is typically used once */
 	uint64_t nonce = 0;
-	/* hash is a 256-bit number */
 	uint8_t hash[SHA256_DIGEST_LENGTH];
 
 	while (1) {
-		/* Update the block's info with the new nonce */
 		block->info.nonce = nonce;
-		/* Compute the hash of the block */
 		block_hash(block, hash);
-		/* Check if the hash matches the difficulty */
+		
 		if (hash_matches_difficulty(hash, block->info.difficulty))
 			break;
-		/* Increment the nonce */
+			
 		nonce++;
 	}
-	/* Copy the resulting hash to the block's data */
+	
+	/* Copy the resulting hash to the block's hash field */
 	memcpy(block->hash, hash, SHA256_DIGEST_LENGTH);
+
+	/* Print the required "Block mined" output line if expected here */
+	printf("Block mined: [%u] ", block->info.difficulty);
+	for (size_t i = 0; i < SHA256_DIGEST_LENGTH; i++)
+	{
+		printf("%02x", hash[i]);
+	}
+	printf("\n");
 }
